2c8a87c582f70d8e91b2c026a85742a6b0c545e6
Akolzin Dmitry <akolzinvs@gmail.com>
2019-01-22 10:00:52 +0300

    Добавлены настройки Daemon Collector

---
+++ src/Block/Frontend/DaemonCollector.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Retailcrm\Retailcrm\Block\Frontend;
+
+class DaemonCollector extends \Magento\Framework\View\Element\Template
+{
+    private $customer;
+    private $helper;
+    private $storeManager;
+    private $storeResolver;
+    private $js = '';
+
+    private $template = <<<EOT
+<script type="text/javascript">
+    (function(_,r,e,t,a,i,l){_['retailCRMObject']=a;_[a]=_[a]||function(){(_[a].q=_[a].q||[]).push(arguments)};_[a].l=1*new Date();l=r.getElementsByTagName(e)[0];i=r.createElement(e);i.async=!0;i.src=t;l.parentNode.insertBefore(i,l)})(window,document,'script','https://collector.retailcrm.pro/w.js','_rc');
+    {{ code }}
+    _rc('send', 'pageView');
+</script>
+EOT;
+
+    /**
+     * DaemonCollector constructor.
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Store\Api\StoreResolverInterface $storeResolver
+     * @param \Retailcrm\Retailcrm\Helper\Data $helper
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Store\Api\StoreResolverInterface $storeResolver,
+        \Retailcrm\Retailcrm\Helper\Data $helper,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->customer = $customerSession->getCustomer();
+        $this->storeManager = $storeManager;
+        $this->storeResolver = $storeResolver;
+        $this->helper = $helper;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJs()
+    {
+        return $this->js;
+    }
+
+    /**
+     * @return $this
+     */
+    public function buildScript()
+    {
+        $params = array();
+
+        if ($this->customer->getId()) {
+            $params['customerId'] = $this->customer->getId();
+        }
+
+        try {
+            $siteKey = $this->helper->getSiteKey(
+                $this->storeManager->getStore(
+                    $this->storeResolver->getCurrentStoreId()
+                )->getWebsiteId()
+            );
+        } catch (\Magento\Framework\Exception\NoSuchEntityException $entityException) {
+            return $this;
+        }
+
+        if ($siteKey) {
+            $this->js = preg_replace(
+                '/{{ code }}/',
+                sprintf(
+                    "\t_rc('create', '%s', %s);\n",
+                    $siteKey,
+                    json_encode((object) $params)
+                ),
+                $this->template
+            );
+        }
+
+        return $this;
+    }
+}
---
+++ src/Test/Unit/Block/Frontend/DaemonCollectorTest.php
@@ -0,0 +1,75 @@
+<?php
+
+class DaemonCollectorTest extends \PHPUnit\Framework\TestCase
+{
+    private $unit;
+    private $customer;
+
+    const SITE_KEY = 'RC-XXXXXXX-X';
+
+    public function setUp()
+    {
+        $objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $context = $objectManager->getObject(\Magento\Framework\View\Element\Template\Context::class);
+        $customerSession = $this->createMock(\Magento\Customer\Model\Session::class);
+        $storeManager = $this->createMock(\Magento\Store\Model\StoreManager::class);
+        $storeResolver = $this->createMock(\Magento\Store\Model\StoreResolver::class);
+        $helper = $this->createMock(\Retailcrm\Retailcrm\Helper\Data::class);
+        $this->customer = $this->createMock(\Magento\Customer\Model\Customer::class);
+        $store = $this->createMock(\Magento\Store\Model\Store::class);
+
+        $customerSession->expects($this->any())
+            ->method('getCustomer')
+            ->willReturn($this->customer);
+        $storeResolver->expects($this->any())
+            ->method('getCurrentStoreId')
+            ->willReturn(1);
+        $store->expects($this->any())
+            ->method('getWebsiteId')
+            ->willReturn(1);
+        $storeManager->expects($this->any())
+            ->method('getStore')
+            ->willReturn($store);
+        $helper->expects($this->any())
+            ->method('getSiteKey')
+            ->willReturn(self::SITE_KEY);
+
+        $this->unit = new \Retailcrm\Retailcrm\Block\Frontend\DaemonCollector(
+            $context,
+            $customerSession,
+            $storeManager,
+            $storeResolver,
+            $helper
+        );
+    }
+
+    public function testGetJSWithCustomer()
+    {
+        $this->customer->expects($this->any())
+            ->method('getId')
+            ->willReturn(1);
+
+        $js = $this->unit->buildScript()->getJs();
+
+        $this->assertContains('<script type="text/javascript">', $js);
+        $this->assertContains('</script>', $js);
+        $this->assertContains('_rc(\'send\', \'pageView\');', $js);
+        $this->assertContains(self::SITE_KEY, $js);
+        $this->assertContains('customerId', $js);
+    }
+
+    public function testGetJSWithoutCustomer()
+    {
+        $this->customer->expects($this->any())
+            ->method('getId')
+            ->willReturn(null);
+
+        $js = $this->unit->buildScript()->getJs();
+
+        $this->assertContains('<script type="text/javascript">', $js);
+        $this->assertContains('</script>', $js);
+        $this->assertContains('_rc(\'send\', \'pageView\');', $js);
+        $this->assertContains(self::SITE_KEY, $js);
+        $this->assertNotContains('customerId', $js);
+    }
+}
