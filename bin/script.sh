95e765d01ea759d12d14846fd2d973b7ee70174c
Akolzin Dmitry <akolzinvs@gmail.com>
2020-04-06 16:36:01 +0300

    Update travis config

--- bin/script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -z $TRAVIS_BUILD_DIR ]; then
-	exit 0;
-fi
-
-MAGE_ROOT=$TRAVIS_BUILD_DIR/../magento2
-cd $MAGE_ROOT
-php vendor/phpunit/phpunit/phpunit -c dev/tests/unit/phpunit.xml.dist app/code/Retailcrm/Retailcrm/Test/Unit
